a0c2a9684ca9c810d1cae64587f98f33c3f87e46
Heitor <heitor.adao@gmail.com>
2022-04-04 18:41:22 -0300

    Add more attributes

--- lib/group.rb
+++ lib/group.rb
@@ -5,6 +5,7 @@ class Group
   attr_accessor :resolved, :muted, :muted_by, :muted_at
   attr_accessor :errors
   attr_accessor :context
+  attr_accessor :last_notice_id
   attr_accessor :last_notice_at, :notice_count, :notice_total_count
   attr_accessor :comment_count
 
@@ -14,6 +15,10 @@ class Group
     self.resolved = data['resolved']
     self.errors = Array.new(data['errors'].size) {|i| Error.new(data['errors'][i]) }
     self.context = data['context']
+    self.last_notice_id = data['lastNoticeId']
+    self.last_notice_at = data['lastNoticeAt']
+    self.notice_count = data['noticeCount']
+    self.notice_total_count = data['noticeTotalCount']
   end
 end
 
