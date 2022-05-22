7d39a8d4ef765870fc6ad8c0d7ca1b97ca3efb2f
Jorge Borges <jorgetrabalho2016@gmail.com>
2022-05-22 00:53:34 -0300

    Ui menu implementation

--- lib/ui/main.rb
+++ lib/ui/main.rb
@@ -24,6 +24,32 @@ module Ui
         puts project_resume
         puts project_emoji
       end
+
+      def menu
+        menu_introduction = "Pulisse Menu"
+        menu_option = "You have 2 options:"
+        load_projects = "Load Projects"
+        select_project = "Select Project or Projects"
+
+        description_entry_number = "Select a option number:"
+
+        puts menu_introduction
+        puts menu_option
+
+        select_option([load_projects, select_project])
+        puts entry_number
+
+        entry_number = gets.chomp
+        entry_number
+      end
+
+      private
+
+      def select_option(options: [])
+        options.each_with_index do |option, option_number|
+          puts "#{option_number++} ) #{option}"
+        end
+      end
     end
   end
 end
