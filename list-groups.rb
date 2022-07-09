0e3c0950ed299e7c116bbc41a6e58f56553977ca
Jorge Borges <jorgetrabalho2016@gmail.com>
2022-07-09 11:55:09 -0300

    WIP - move require gems for main file

--- list-groups.rb
+++ list-groups.rb
@@ -5,27 +5,18 @@
 
 # $: << 'lib'
 
-require 'pry'
-require 'faraday'
-require 'json'
-
-Dir.glob('lib/*.rb').each { |lib| load lib }
-
-require 'dotenv'
-Dotenv.load
-
 def get_cached_json
   File.read('data/cached.json')
 end
 
 def get_live_info
   key = ENV['AIRBRAKE_KEY']
-  project_id = ENV['PROJECT_ID']
+  project_id = nil
   params = { key: key }
   # url = "https://api.airbrake.io/api/v4/groups"
   url = "https://api.airbrake.io/api/v4/projects/#{project_id}/groups"
   response = Faraday.get(url, params)
-  File.write('data/cached.json', response.body)
+  File.write('./data/cached.json', response.body)
   response.body
 end
 
@@ -35,12 +26,12 @@ raw_json = get_live_info
 begin
   h = JSON.parse(raw_json)
   groups = h['groups'].map {|g| Group.new(g) }
-  my_fault = groups.select do |g|
-    #g.blame_include?('Heitor') ||
-    g.files_include?('advance')
-  end
+  my_fault =
+    groups.select do |g|
+      #g.blame_include?('Heitor') ||
+      g.files_include?('advance')
+    end
 
-  binding.pry
 rescue JSON::ParserError => e
   puts e.message
 end
