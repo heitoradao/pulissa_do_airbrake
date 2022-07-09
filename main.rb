3664fcfb2991c9e75226e8fe6119aa8328d3bce1
Jorge Borges <jorgetrabalho2016@gmail.com>
2022-07-09 12:39:36 -0300

    WIP - Add loop menu

--- main.rb
+++ main.rb
@@ -12,4 +12,9 @@ Dir.glob('lib/**/*.rb').each { |lib| load lib }
 Ui::Main.introduction
 option_number = Ui::Main.menu
 
-Ui::Main.presentation_of_option(option_number)
+loop do
+  Ui::Main.presentation_of_option(option_number)
+  break if option_number == 3
+
+  option_number = Ui::Main.menu
+end
